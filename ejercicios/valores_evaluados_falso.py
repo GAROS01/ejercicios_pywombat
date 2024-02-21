3e21023ab80ba1884c83d541fb1098246c8b5cd9
GAROSDEV <garzonoscar7@gmail.com>
2024-02-21 13:34:14 -0500

    Agrego ejercicio de los valores registrados falsos

---
+++ ejercicios/valores_evaluados_falso.py
@@ -0,0 +1,25 @@
+"""
+Diga de los siguientes valores, cuales son evaluados como falsos en Python. Argumente su respuesta imprimiendo en consola los valores, demostrando que éstos son falsos.
+
+Pista:
+
+Son 5 opciones
+
+0
+""
+'False'
+"0"
+[]
+False
+None
+Ejemplo:
+
+print(None or False)  # False
+"""
+
+# Demostración de valores evaluados como falsos
+print(bool(0))
+print(bool(""))
+print(bool([]))
+print(bool(False))
+print(bool(None))
