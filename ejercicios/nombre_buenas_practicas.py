1c9f0ae52ae148080823588612d5a5da162658e4
GAROSDEV <garzonoscar7@gmail.com>
2024-02-22 13:32:55 -0500

    otro ejercicio

---
+++ ejercicios/nombre_buenas_practicas.py
@@ -0,0 +1,31 @@
+"""
+En todos los lenguajes de programación existen muchos estilos de nombres diferentes. Es útil poder reconocer qué estilo de nomenclatura se está utilizando, independientemente de para qué se utilizan.
+
+Detalles:
+
+Por convención todo nuestro código debe estar en inglés
+
+Siguiendo el estilo de PEP 8, escriba:
+
+Nombre para la constante de masa atómica.
+Nombre de variable para: “costo del producto”, “total del producto”.
+Nombre para la clase de “elaboración de alimentos”.
+Nombre para el método “obtener productos disponibles”, “buscar producto”.
+"""
+
+ATOMIC_MAS = 0  # muc**2
+
+product_cost = 0
+total_product = 0
+
+
+class FoodPreparation:
+    pass
+
+
+def get_available_products():
+    pass
+
+
+def search_product():
+    pass
